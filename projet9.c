5707436212e2b0221945f8fd7733d242e89dc7ff
youssefenafaa <youssefnafaa2000@gmail.com>
2025-09-24 18:18:59 +0200

    finnished

--- projet9.c
+++ projet9.c
@@ -42,11 +42,29 @@ int main () {
 
      Chiffre [0] =isbn[16];
 
-    printf("GS1 : %s\n", GS1);
-    printf("Identifiant : %s\n", Identifiant);
-    printf("Code : %s\n", Code);
-    printf("Numero : %s\n", Numero);
-    printf("Chiffre de contrôle : %s\n", Chiffre);
+    printf("GS1 : ");
+    for (int i = 0; i < 3; i++)
+     {
+        printf("%c",GS1[i]);
+     }
+    printf("Identifiant :");
+
+    printf("%c",Identifiant[i] ) ;
+    
+    printf("Code :");
+     for ( i = 6; i < 9 ; i++)
+     {
+      printf("%c",Code[i]);
+     }
+    printf("Numero : " );
+     for ( i = 10 ; i < 15; i++)
+       {
+
+      printf("%c",Numero[i] );
+
+       }
+    printf("Chiffre de controle :");
+     printf("%c", Chiffre[i]);
      
     
 
