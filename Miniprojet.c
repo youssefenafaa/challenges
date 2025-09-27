d555adf385e2c963ec638bfe2051fcf2b9b8f302
youssefenafaa <youssefnafaa2000@gmail.com>
2025-09-27 16:08:55 +0100

    Update Miniprojet.c

--- Miniprojet.c
+++ Miniprojet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #define Max_etudiant 30
-#define Max_notesn 4
+#define Max_notes 4
 
 
 typedef struct {
@@ -12,17 +12,137 @@ typedef struct {
       float moyenne;
       } Etudiant;
 
-Etudiant Class [30] ;
+Etudiant classe [30];
 int nbEtudiants = 0;
 void ajouter(){
-    if (nbEtudiants>=  )
+    if (nbEtudiants>= Max_etudiant ){
+    printf("Limite atteinte \n");
+    return;
 }
+Etudiant E;
+printf("cne :");
+scanf("%s",E.cne);
+printf("nom :");
+scanf("%s",E.nom);
+printf("prenom :");
+scanf("%s",E.prenom);
+
+for (int i=0;i<4;i++);
+{
+    E.notes[i]=0;
+}
+   E.moyenne=0
+ classe[nbEtudiants]=E;
+    nbEtudiants++;
+printf("Étudiant ajouté\n");
+
+}
+void saisirNotes(){
+if (nbEtudiants == 0)
+{
+    printf("Aucun etudiant ajoute\n");
+    return;
+}
+ char  cne[20];
+    printf("entrer le cne de l etudiant :\n");
+    scanf("%s",cne);
+    int trouve=0
+    for (int i=0; i <nbEtudiants;i++);
+    {
+        if (strcmp(classe[i].cne, cne) == 0)
+        for (int j=0;j<4;j++)
+        {
+             do {
+                    printf("Note %d : ", j + 1);
+                    scanf("%f", &classe[i].notes[j]);
+                } while (classe[i].notes[j] < 0 || classe[i].notes[j] > 20);
+        }
+        
+    }
+    
+void calculerMoyenneEtudiant(){
+float somme = 0;
+            for (int j = 0; j < 4; j++) {
+                somme += classe[i].notes[j];
+            }
+            classe[i].moyenne = somme / 4;
+
+            printf("Moyenne calculée : %.2f\n", classe[i].moyenne);
+            trouve = 1;
+            break;
 
 
+            if (!trouve) {
+        printf("Étudiant introuvable.\n");}
+    }
+ 
+}
+void calculerMoyenneGenerale(){
+      if( nbEtudiants == 0)
+{
+      printf("Aucun etudiant \n");
+      return;
+}
+float somme = 0;
+            for (int j = 0; j < 4; j++) {
+                somme += classe[i].notes[j];
+            }
+            
+            
+            float moyenneGen = somme / 4;
+          printf("Moyenne generale de la classe : %.2f\n", moyenneGen);
 
 
+}
+void afficherEtudiants() {
+    if (nbEtudiants == 0) {
+        printf("Aucun étudiant à afficher.\n");
+        return;
+
+      }
+
+ printf("\nListe des étudiants :\n");
+
+ for (int i = 0; i < 4; i++) {
+        printf("%s | %s %s | [", classe[i].cne, classe[i].nom, classe[i].prenom);
+        for (int j = 0; j < Max_notes;j++) ;
+            printf("%.2f", classe[i].notes[j]);
+            if (j <Max_notes -1)printf(", ");
+        }
+
+       printf("] | Moyenne: %.2f\n", classe[i].moyenne);
 
 
+}    
+void afficherBulletin(){
+
+ char cne[20];
+    printf("Entrez le CNE d etudiant");
+    scanf("%s", cne);
+
+    int trouve = 0;
+   for (int i = 0; i < nbEtudiants; i++) {
+     if (strcmp(classe[i].cne, cne) == 0) {
+            printf("Bulletin:\n");
+            printf("CNE : %s\n", classe[i].cne);
+            printf("Nom : %s\n", classe[i].nom);
+            printf("Prénom : %s\n", classe[i].prenom);
+            printf("Notes : "); ; 
+           for (int j = 0; j < Max_notes ; j++) {
+                printf("%.2f ", classe[i].notes[j]);
+            }
+
+            printf("Moyenne : %.2f\n", classe[i].moyenne);
+            trouve = 1;
+            break;
+
+            if (!trouve) {
+    printf("Aucun Etudiant\n");
+
+          }
+        }
+    }
+ }
 
 int main (){
 int option; 
@@ -63,9 +183,9 @@ switch ( option){
     afficherBulletin()
     break;
     case 0 :
-    Quitter()
+   printf("Au revoir \n");
     break;
-default:
+default:printf("Choix invalide\n");
     break;
 }
 
@@ -77,4 +197,4 @@ return 0;
 
 
 }
-}
+} 
